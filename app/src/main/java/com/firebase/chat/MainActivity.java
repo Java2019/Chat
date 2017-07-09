8759a24c5cfe0b7f6d28d2878bf55098a839bcba
java2019 <java_2019@mail.ru>
2017-07-09 19:58:17 +0300

    libs

--- app/src/main/java/com/firebase/chat/MainActivity.java
+++ app/src/main/java/com/firebase/chat/MainActivity.java
@@ -1,21 +1,24 @@
 package com.firebase.chat;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
 
     private int SIGN_IN_REQUEST_CODE = 1;
+    //private FirebaseApp firebaseApp = FirebaseApp.initializeApp(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+
+        if(FirebaseAuth.getInstance(FirebaseApp.initializeApp(getApplicationContext())).getCurrentUser() == null) {
             // Start sign in/sign up activity
             startActivityForResult(
                     AuthUI.getInstance()
